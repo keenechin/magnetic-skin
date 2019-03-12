f9f9f72c3f81feda0f8b7e6d27566b2a5fc05d14
Keene Chin <keene.chin@gmail.com>
2019-03-12 00:43:40 +0000

    loaded all txt files into single dataframe

--- make-dataset.py
+++ make-dataset.py
@@ -8,7 +8,7 @@ if sys.version_info[0] < 3:
 else:
     from io import StringIO
 
-#%% Get filenemaes
+#%% Get filenames
 def getFilenames(dirname = "../data"):
     filenames = []
     for file in os.listdir(dirname):
@@ -19,39 +19,61 @@ def getFilenames(dirname = "../data"):
 filenames = getFilenames()
 
 #%% Line by line get dataset
+frames = []
+labels = ['test_num', 'location', 'x', 'y', 'z', 't', 'weight']
+
 for name in filenames:
     file = open(name,'r')
     data = file.readlines()
+    test = []
+    lastnum = 0
+    tests = []
     for datum in data:
         datum = datum.replace("[","")
         datum = datum.replace("]","")
-        datum = datum.split(",")
-        print(datum)
+        datum = datum.replace("\n","")
+        datum = datum.split(", ")
+        testnum = datum[0]
+
+        if testnum != lastnum:
+            tests.append(test)
+            test = []
+            lastnum = testnum
+        test.append(tuple(datum))
+    tests.append(test)
+    tests = np.array(tests[3:])
+    for test in tests:
+        frame = pd.DataFrame.from_records(test,columns=labels)
+        frames.append(frame)
+
+dataset = pd.concat(frames)
+dataset.to_pickle('../data/all_data.pkl')
 
 
-#%% Get dataset
-frames = []
-for name in filenames:
-    file = open(name,'r')
-    datastring = file.read()
-    datastring = datastring.replace("[","")
-    datastring = datastring.replace("]","")
-    print(datastring)
-    frame = pd.read_csv(StringIO(datastring))
-    print(frame.shape)
-    frames.append(frame)
-dataset = np.array(pd.concat(frames))
+if False:
+    #%% Get dataset
+    frames = []
+    for name in filenames:
+        file = open(name,'r')
+        datastring = file.read()
+        datastring = datastring.replace("[","")
+        datastring = datastring.replace("]","")
+        print(datastring)
+        frame = pd.read_csv(StringIO(datastring))
+        print(frame.shape)
+        frames.append(frame)
+    dataset = np.array(pd.concat(frames))
 
 
-#%% Get dataset
-frames = []
-for name in filenames:
-    frame = pd.read_csv(name,sep=",",engine='python')
-    array = np.array(frame)
-    frames.append(pd.DataFrame(array))
+    #%% Get dataset
+    frames = []
+    for name in filenames:
+        frame = pd.read_csv(name,sep=",",engine='python')
+        array = np.array(frame)
+        frames.append(pd.DataFrame(array))
 
 
-dataset = np.array(pd.concat(frames))
+    dataset = np.array(pd.concat(frames))
 
 
 #%%
