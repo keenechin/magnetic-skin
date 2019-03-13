d667de4b60a788bf0a5285350f2be312549aed56
Keene Chin <keene.chin@gmail.com>
2019-03-13 14:18:48 +0000

    feature extraction, need to change data types

--- timeseries.py
+++ timeseries.py
@@ -1,7 +1,11 @@
 #%% 
 import numpy as np
 import pandas as pd
-import tsfresh
+from tsfresh import extract_relevant_features
+from tsfresh.examples.robot_execution_failures import download_robot_execution_failures, \
+    load_robot_execution_failures
+download_robot_execution_failures()
+timeseriese, ye = load_robot_execution_failures()
 
 #%% Load data
 data=np.load('../data/dataset.npy')
@@ -9,5 +13,14 @@ labels = ['test_num', 'location', 'x', 'y', 'z', 't', 'weight']
 dataset = pd.DataFrame({ labels[0]:data[:,0] , labels[1]:data[:,1] , labels[2]:data[:,2], \
 labels[3]:data[:,3] , labels[4]:data[:,4] , labels[5]:data[:,5] , labels[6]:data[:,6]})
 
-#%%
+#%% Access the data method
+def getTest(dataset,num):
+    return dataset[dataset['test_num']==num]
 
+#%% Extract class vector
+
+
+y = pd.Series(y.astype(int))
+#%% Extract relevant features
+
+features =  extract_relevant_features(dataset, y, column_id='test_num', column_sort='t')
