6493d6c7f4d8a1f66c464f3651e0392361ce778d
Keene Chin <keene.chin@gmail.com>
2019-03-13 21:18:36 +0000

    evaluate with confusion matrix

--- classification.py
+++ classification.py
@@ -1,10 +1,9 @@
 #%% Imports
-from sklearn.pipeline import Pipeline
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import ShuffleSplit
-from tsfresh.transformers import RelevantFeatureAugmenter
+from sklearn.metrics import confusion_matrix
 import pandas as pd
 import numpy as np
 
@@ -14,29 +13,28 @@ y = np.load('../data/y.npy')
 y = pd.Series(y.astype(int))
 N = len(X)
 
-
-#%% Train-Test split with shuffle
 rs = ShuffleSplit(n_splits=5, test_size=.25, random_state=8)
 for tr,tst in rs.split(X):
     train_x = np.array(X)[tr]
     train_y = np.array(y)[tr]
     test_x = np.array(X)[tst]
     test_y = np.array(y)[tst]
+#%% Eval function
+def evalModel(model,train_x,train_y,test_x,test_y):
+    y_hat = model.predict(test_x)
+    print('Predicted classes: {0}'.format(np.unique(y_hat)))
+    print('Training score: {0}'.format(model.score(train_x,train_y)))
+    print('Test score: {0}'.format(model.score(test_x,test_y)))
+    plt.matshow(confusion_matrix(test_y,y_hat))
 #%% Random Forest Classification
 forest = RandomForestClassifier(n_estimators=100)
 forest.fit(train_x,train_y)
-print('Training score: {0}'.format(forest.score(train_x,train_y)))
-print('Test score: {0}'.format(forest.score(test_x,test_y)))
-
 #%% KNN
 knn = KNeighborsClassifier(n_neighbors=1)
 knn.fit(train_x,train_y)
-print('Training score: {0}'.format(knn.score(train_x,train_y)))
-print('Test score: {0}'.format(knn.score(test_x,test_y)))
+evalModel(knn,train_x,train_y,test_x,test_y)
 
 #%% MLP
-mlp = MLPClassifier()
+mlp = MLPClassifier(solver = 'adam', hidden_layer_sizes=(38,38),max_iter=int(1e4),random_state=8)
 mlp.fit(train_x,train_y)
-print('Training score: {0}'.format(mlp.score(train_x,train_y)))
-print('Test score: {0}'.format(mlp.score(test_x,test_y)))
-#%%
+evalModel(mlp,train_x,train_y,test_x,test_y)
